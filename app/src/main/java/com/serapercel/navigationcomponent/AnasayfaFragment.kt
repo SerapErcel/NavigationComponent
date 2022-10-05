174cd80ccd8b802c598312b143ab61f18ed43804
Serap <ercelserap@gmail.com>
2022-10-05 22:09:36 +0300

    Add Navigation

--- app/src/main/java/com/serapercel/navigationcomponent/AnasayfaFragment.kt
+++ app/src/main/java/com/serapercel/navigationcomponent/AnasayfaFragment.kt
@@ -5,55 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.google.android.material.snackbar.Snackbar
+import com.serapercel.navigationcomponent.databinding.FragmentAnasayfaBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [AnasayfaFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class AnasayfaFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var tasarim:FragmentAnasayfaBinding
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_anasayfa, container, false)
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        tasarim = FragmentAnasayfaBinding.inflate(inflater, container, false)
+
+        tasarim.butonBasla.setOnClickListener{
+            Snackbar.make(it,"Merhaba",Snackbar.LENGTH_SHORT).show()
+            Navigation.findNavController(it).navigate(R.id.oyunEkraninaGecis)
+        }
+        return tasarim.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment AnasayfaFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            AnasayfaFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
--- app/src/main/java/com/serapercel/navigationcomponent/MainActivity.kt
+++ app/src/main/java/com/serapercel/navigationcomponent/MainActivity.kt
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    /*  ----- FRAGMENT -----
+    Activity moduler bolumudur
+    Bir Activity birden fazla fragment calistirabilir
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
